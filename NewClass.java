30093ee51a3cb97bc1ac6a0e56debd8143f6cd3e
na69300 <69648473+na69300@users.noreply.github.com>
2020-08-13 11:59:24 -0700

    Add files via upload

---
+++ NewClass.java
@@ -0,0 +1,12 @@
+class NewClass
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+        System.out.println("I like to play basketball");
+        System.out.println("I want to major in computer science");
+        System.out.println("I want to attend SJSU");
+        System.out.println("Favorite video game is Call of Duty Mobile");
+        System.out.println("I hope to learn a lot in this class");
+    }
+}
